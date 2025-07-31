a934df0b7eec5343b9c2d9fa2d65096338e06872
Tyler Beckman <ty@myriation.xyz>
2025-07-31 00:10:09 -0600

    Make nix packaging not require hashes w/ pnpm2nix & custom pnpm based on packageManager key

--- flake.nix
+++ flake.nix
@@ -2,9 +2,16 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
         flake-utils.url = "github:numtide/flake-utils";
+        pnpm2nix = {
+            url = "github:FliegendeWurst/pnpm2nix-nzbr";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+                flake-utils.follows = "flake-utils";
+            };
+        };
     };
 
-    outputs = { self, nixpkgs, flake-utils, ... }:
+    outputs = { self, nixpkgs, flake-utils, pnpm2nix, ... }:
         flake-utils.lib.eachDefaultSystem (system: let
             # Initialize nixpkgs
             pkgs = nixpkgs.legacyPackages.${system};
@@ -12,7 +19,9 @@
         in {
             packages = {
                 default = self.packages.${system}.website;
-                website = pkgs.callPackage ./nix/package.nix {};
+                website = pkgs.callPackage ./nix/package.nix {
+                    inherit (pnpm2nix.packages.${system}) mkPnpmPackage;
+                };
             };
             devShells.default = pkgs.mkShellNoCC {
                 # Add all build-time dependencies to the environment
--- nix/package.nix
+++ nix/package.nix
@@ -1,48 +1,50 @@
 {
-    stdenv,
-    nodejs,
+    # nixpkgs dependencies
     lib,
+    nodejs,
     callPackage,
-    corepackHooks ? (callPackage ./corepack-hooks.nix {}),
 
-    # Overridables
-    corepackCacheHash ? "sha256-gEIS3bFYznzZJsyPdZjJ+3lULPoJ9owe05OrEHvgWkM=",
-    pnpmDepsHash ? "sha256-gPjZ06byWolGzMrhArPRhQIfITpB2GqnV0uD2+vI5vs=",
+    # non-nixpkgs dependencies
+    mkPnpmPackage, # https://github.com/FliegendeWurst/pnpm2nix
+    corepackCompat ? callPackage ./corepack-compat.nix {},
+    customPnpm ? corepackCompat.pnpmFromPackageJson ../package.json,
+
+    # overridables
+    originalSrc ? ./..
 }:
-stdenv.mkDerivation (final: {
+mkPnpmPackage {
     pname = "polyfrost-website";
     version = "0";
 
-    src = ./..;
-
-    nativeBuildInputs = [
-        nodejs
-        corepackHooks.setupHook
-        corepackHooks.pnpm.configHook
-    ];
-
-    corepackCache = corepackHooks.corepackCache {
-        hash = corepackCacheHash;
-        inherit (final) pname src;
-        inherit nodejs;
+    # Setup pnpm package source
+    src = lib.sources.cleanSourceWith {
+        name = "website-src";
+        src = lib.sources.cleanSourceWith {
+            src = originalSrc;
+            filter =
+                name: type:
+                let
+                    baseName = baseNameOf (toString name);
+                in
+                !(builtins.elem baseName [ "dist" "node_modules" ".astro" ]);
+        };
+        filter = lib.sources.cleanSourceFilter;
     };
+    packageJSON = ../package.json;
+    pnpmLockYaml = ../pnpm-lock.yaml;
+    pnpmWorkspaceYaml = ../pnpm-workspace.yaml;
+    components = [ "./apps/website" ];
 
-    pnpmWorkspaces = [ "@polyfrost/website" ];
-    pnpmDeps = corepackHooks.pnpm.fetchDeps {
-        hash = pnpmDepsHash;
-        inherit (final) pname version src pnpmWorkspaces;
-        fetcherVersion = 2;
-    };
+    inherit nodejs;
+    pnpm = customPnpm;
 
-    buildPhase = ''
-        runHook preBuild
-
-        export ASTRO_TELEMETRY_DISABLED="1"
-
-        pnpm run --filter @polyfrost/website build
-
-        runHook postBuild
+    scriptFull = ''
+        pnpm website build
     '';
+
+    buildEnv = {
+        ASTRO_TELEMETRY_DISABLED = "1";
+    };
 
     installPhase = ''
         runHook preInstall
@@ -50,12 +52,12 @@ stdenv.mkDerivation (final: {
         mkdir -p "$out"/{bin,lib}
 
         # Copy all the astro files to the output
-        cp -r ./apps/website/dist/* "$out"/lib/
+        cp -r ./apps/website/dist/* "$out"/share/
 
         # Make a wrapper command to start the server
         cat << EOF > "$out"/bin/start-server
         #!/usr/bin/env bash
-        ${lib.getExe nodejs} "$out"/lib/server/entry.mjs
+        ${lib.getExe nodejs} "$out"/share/server/entry.mjs
         EOF
         chmod +x "$out"/bin/start-server
 
@@ -65,7 +67,7 @@ stdenv.mkDerivation (final: {
     meta = {
         mainProgram = "start-server";
         description = "The nodejs code to run polyfrost's website";
-        homepage = "https://github.com/Polyfrost/Nexus";
+        homepage = "https://github.com/Polyfrost/website";
         license = lib.licenses.agpl3Only;
     };
-})
+}
